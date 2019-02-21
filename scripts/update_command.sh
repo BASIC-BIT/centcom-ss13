81b8e44cbde1f1b7fe2ce20dca97921490bb2aa4
Steven Kneisler <stevenkneisler@gmail.com>
2019-02-20 19:41:28 -0500

    More init nonsense

--- scripts/update_command.sh
+++ scripts/update_command.sh
@@ -1,1 +1,1 @@
-terraform apply -var 'prod_lambda_deploy_version=0.2.8' -var 'dev_lambda_deploy_version=0.2.8'
+terraform apply -var 'prod_lambda_deploy_version=0.2.9' -var 'dev_lambda_deploy_version=0.2.9'
--- server/sql/initTables.sql
+++ server/sql/initTables.sql
@@ -2,6 +2,6 @@ CREATE DATABASE centcom;
 
 USE centcom;
 
-CREATE TABLE servers (name VARCHAR(50), url VARCHAR(100), description VARCHAR(200));
+CREATE TABLE servers (name VARCHAR(50), url VARCHAR(100), description VARCHAR(200), access_level VARCHAR(20));
 
 CREATE TABLE config (cfg_key VARCHAR(50) UNIQUE, cfg_value VARCHAR(200));
