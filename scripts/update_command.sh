9a007e7d167b591bdb9303ab6fa471859bd38dc6
Steven Kneisler <stevenkneisler@gmail.com>
2019-03-01 19:32:57 -0500

    0.3.1

--- scripts/update_command.sh
+++ scripts/update_command.sh
@@ -1,1 +1,1 @@
-terraform apply -var 'prod_lambda_deploy_version=0.2.53' -var 'dev_lambda_deploy_version=0.2.53'
+terraform apply -var 'prod_lambda_deploy_version=0.3.1' -var 'dev_lambda_deploy_version=0.3.1'
